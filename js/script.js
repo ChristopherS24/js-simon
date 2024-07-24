5e24b8514a010ec8e5a3207235edc292e1009812
ChristopherS24 <chryskies@gmail.com>
2024-07-25 01:56:18 +0200

    random e timer inseriti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 //creazione 5 numeri random
+Numbers = 5;
+const randomNumber = [];
+for (let i = 0; i < Numbers; i ++) {
+    const randomNumber = generateRandomNumber(1, 100);
+    console.log('randomNumber', randomNumber, typeof randomNumber);
+}
+
+
+//timer
+setTimeout(timer, 30000);
+function timer() {
+    alert('STOP');
+}
+
